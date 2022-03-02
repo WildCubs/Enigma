ef5450d6b3b88a49f3a5a111597acc5f37c7651e
WildCubs <phil379c@stud.kea.dk>
2022-03-02 14:07:54 +0100

    Starting to work

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,6 +7,7 @@ public class Main {
     Scanner keybdINput = new Scanner(System.in);
     String sourceText;
     boolean inDecryptionMode = false;
+    //String[] arrayedCode = new String[10];
 
     public char[] alphabet = {' ', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'Æ', 'Ø', 'Å'};
     public int[] numberCiphers = new int[30];
@@ -34,78 +35,85 @@ public class Main {
         return revertedOutput;
     }
 
-    public String[] getSourceText() {
-        System.out.println("Please input first source text or stub");
-        sourceText = keybdINput.nextLine();
+    public void displayOutput(String output) {
+        System.out.println("Result: " + output);
+    }
 
-        String[] arrayedCode = new String[10];
-        for (int i = 0; i < 11; i++) {
-            if (sourceText = "ENIGMA") {
+    public String useNumberCipher(int shift) {
+        System.out.println("Enter text to encrypt:");
+        String input = keybdINput.next();
+        StringBuilder encryptedText = new StringBuilder();
+
+        for (int i = 0; i < input.length(); i++) {
+            encryptedText.append(letter2number(input.charAt(i)) + shift);
+            encryptedText.append(".");
+        }
+        return encryptedText.toString();
+    }
+
+    public String useVigenéreCipher(String keyword) {
+        if (inDecryptionMode = false);
+        System.out.println("Enter text to encrypt:");
+        String input = keybdINput.next();
+        StringBuilder encryptedText = new StringBuilder();
+
+        for (int i = 0; i < input.length(); i++) {
+            int factor = letter2number(input.charAt(i));
+            int newPosition = i + factor;
+            char letter = number2letter(newPosition);
+            encryptedText.append(letter);
+        }
+        return encryptedText.toString();
+    }
+
+    public String useCaesarCipher(int shift) {
+        if (inDecryptionMode = false);
+        System.out.println("Enter text to encrypt");
+        String input = keybdINput.nextLine();
+        StringBuilder encryptedText = new StringBuilder();
+
+        for (int i = 0; i < input.length(); i++) {
+            int mutant = letter2number(input.charAt(i));
+            mutant += shift;
+            char letter = number2letter(mutant);
+            encryptedText.append(letter);
+        }
+        return encryptedText.toString();
+    }
+
+    public void mainMenu() {
+        System.out.println("Choose next encryption wheel: (1) Caesar Cipher, (2) Vigenère Cipher, (3) Number Cipher");
+        int userChoice = keybdINput.nextInt();
+
+        switch (userChoice) {
+            case 1 :
+                System.out.println("Enter shift value:");
+                int shift = keybdINput.nextInt();
+                String result = useCaesarCipher(shift);
+                displayOutput(result);
                 break;
-            }
-            arrayedCode[i] = sourceText;
-            return arrayedCode;
-        }
-
-        System.out.println("Enter next source stub, or type ENIGMA to encrypt!");
-    }
-
-    /*
-    public char installFromVigenèreWheel(boolean inDecryptionMode) {
-
-        return modifiedValue;
-    }
-
-    public char installFromCaesarWheel(boolean inDecryptionMode) {
-        char modifiedValue =  ;
-        return modifiedValue;
-    }
-
-    public String installFromNumberWheel(boolean inDecryptionMode, char input) {
-
-        String modifiedOutput;
-
-        for (int i = 0; i <= getSourceText().length; i++) {
-            if (inDecryptionMode = false);
-            modifiedValue = letter2number(input);
-
-        }
-        return modifiedOutput;
-    }
-
-    public void wheelMenu() {
-        System.out.println("Choose next codewheel: (1) Number Cipher, (2)Caesar Cipher, (3)Vigenère Cipher");
-
-        int wheelChoice = keybdINput.nextInt();
-        StringBuilder codedText = new StringBuilder();
-
-        switch (wheelChoice) {
-            case 1:
-                codedText.append(installFromNumberWheel);
-            case 2:
-                installFromCaesarWheel;
-            case 3:
-                installFromVigenèreWheel;
-
+            case 2 :
+                System.out.println("Enter keyword:");
+                String keyword = keybdINput.nextLine();
+                useVigenéreCipher(keyword);
+                break;
+            case 3 :
+                System.out.println("Enter shift value:");
+                shift = keybdINput.nextInt();
+                useNumberCipher(shift);
+                break;
         }
     }
-/*
-    public void displayDirectionMenu() {
-        System.out.println("ENIGMA - recursive encryption");
-        System.out.println("-----------------------------");
-        System.out.println("Do you want to (E)ncrypt, or (D)ecrypt?");
 
-        String sendOrReceiveChoice = keybdINput.next();
-        if ("E".equals(sendOrReceiveChoice)) {
-            wheelMenu(false);
-        } else {                                    // anything else
-            wheelMenu(true);
-        }
-    }
-*/
     public static void main(String[] args) {
-	Main getMe = new Main();
-        System.out.println("Type sourcetext");
-        System.out.println(getMe.getSourceText());
+        Main getMe = new Main();
+        getMe.mainMenu();
+        //getMe.displayOutput(getMe.useCaesarCipher(3));
+        //getMe.displayOutput(getMe.useNumberCipher(1));
+        //getMe.displayOutput(getMe.useVigenéreCipher("ABC"));
     }
 }
+
+
+
+
